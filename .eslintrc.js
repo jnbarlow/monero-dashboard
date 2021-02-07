56166bc219d1f9d92a946fc09fc1cd7ab22b900e
John Barlow <jnbarlow@gmail.com>
2021-02-07 01:33:52 -0500

    Added linter/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	root: true,
+	parser: 'babel-eslint',
+	env: { es6: true },
+	parserOptions: {
+		ecmaVersion: 9,
+		sourceType: 'script'
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	rules: {
+		eqeqeq: ['error', 'smart'],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		semi: ['error', 'always'],
+		'no-cond-assign': ['error', 'always'],
+		'prettier/prettier': 'error',
+		'no-restricted-imports': [
+			'error'
+		]
+	},
+	plugins: ['prettier']
+};
---
+++ .huskyrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	hooks: {
+		'pre-commit': [
+			'pretty-quick --staged --pattern "{**/,}*.{js,jsx,css,less,scss,json,graphql,gql,md,yaml,yml,html,webmanifest,babelrc,prettierrc}"',
+			'eslint --cache --fix .'
+		].join(' && ')
+	}
+};
