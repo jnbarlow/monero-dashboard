a63b588afbb39b4233b20cc15fd56300eaa91d98
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2021-02-09 18:48:08 -0700

    Add prettier bytes view on the connections card

--- client/src/components/cards/ConnectionsCard.js
+++ client/src/components/cards/ConnectionsCard.js
@@ -3,6 +3,15 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function ConnectionsCard(props) {
+    // Shamelessly copied from SO. Make the bytes easy to read.
+    // https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string/10420404
+    function humanBytes(bytes) {
+        var i = bytes == 0 ? 0 : Math.floor(Math.log(bytes) / Math.log(1024));
+        return (
+            (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'KiB', 'MiB', 'GiB', 'TiB'][i]
+        );
+    }
+
     return (
         <Card className="dataCard connections">
             <Card.Body>
@@ -10,22 +19,22 @@ function ConnectionsCard(props) {
                 <Card.Text>
                     <Row className="connectionsHeader">
                         <Col md="2">IP</Col>
-                        <Col md="2">Incoming</Col>
+                        <Col md="2">Direction</Col>
                         <Col md="2">Current Upload</Col>
                         <Col md="2">Current Download</Col>
-                        <Col md="2">Send Count</Col>
-                        <Col md="2">Receive Count</Col>
+                        <Col md="2">Sent Bytes</Col>
+                        <Col md="2">Received Bytes</Col>
                     </Row>
                     <div className="connectionsData">
                         {props.connections.map((conn, index) => {
                             return (
                                 <Row>
                                     <Col md="2">{conn.ip}</Col>
-                                    <Col md="2">{conn.incoming ? 'True' : 'False'}</Col>
-                                    <Col md="2">{conn.current_upload}</Col>
-                                    <Col md="2">{conn.current_download}</Col>
-                                    <Col md="2">{conn.send_count}</Col>
-                                    <Col md="2">{conn.recv_count}</Col>
+                                    <Col md="2">{conn.incoming ? 'Inbound' : 'Outbound'}</Col>
+                                    <Col md="2">{humanBytes(conn.current_upload) + '/s'}</Col>
+                                    <Col md="2">{humanBytes(conn.current_download) + '/s'}</Col>
+                                    <Col md="2">{humanBytes(conn.send_count)}</Col>
+                                    <Col md="2">{humanBytes(conn.recv_count)}</Col>
                                 </Row>
                             );
                         })}
